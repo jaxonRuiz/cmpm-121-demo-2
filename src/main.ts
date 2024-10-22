0b8e7cba62f0b262c00ff37feb84a3aa941bc462
Jaxon Ruiz <45025385+jaxonRuiz@users.noreply.github.com>
2024-10-21 17:36:34 -0700

    added canvas and header

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,16 @@
 import "./style.css";
 
-const APP_NAME = "World";
+const APP_NAME = "Paint World";
 const app = document.querySelector<HTMLDivElement>("#app")!;
+const title = document.createElement("h1")!;
+
+// setting up canvas
+const canvas = document.createElement("canvas")!;
+const ctx = canvas.getContext("2d")!;
+ctx.fillStyle = "white";
 
 document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+title.innerHTML = APP_NAME;
+
+app.append(title);
+app.append(canvas);
