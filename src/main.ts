95089a5a33d669d15cf33e228d6fefb1720b1107
Jaxon Ruiz <45025385+jaxonRuiz@users.noreply.github.com>
2024-10-21 19:03:54 -0700

    checkpoint, half added line tracker

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
 import "./style.css";
 
+// cant figure out how to use multiple files
+interface Point {
+  x: number;
+  y: number;
+};
+
+interface Line {
+  points: Point[];
+}
+
 const APP_NAME = "Paint World";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 const title = document.createElement("h1")!;
 const toolbar_container = document.createElement("div")!;
 document.title = APP_NAME;
 title.innerHTML = APP_NAME;
+let currentLine: Point[] = [];
+const lines: Line[] = [];
 
 // setting up canvas
 const paint_canvas = document.createElement("canvas")!;
@@ -18,7 +30,8 @@ ctx.fillStyle = "white";
 const clearButton = document.createElement("button")!;
 clearButton.innerHTML = "Clear";
 clearButton.addEventListener("click", () => {
-    ctx.clearRect(0, 0, paint_canvas.width, paint_canvas.height);
+  paint_canvas.dispatchEvent(new Event("drawing-changed"));
+  ctx.clearRect(0, 0, paint_canvas.width, paint_canvas.height);
 });
 
 // adding elements to the app
@@ -34,19 +47,47 @@ paint_canvas.addEventListener("mousedown", (e) => {
   cursor.active = true;
   cursor.x = e.offsetX;
   cursor.y = e.offsetY;
+
+  currentLine = [];
+  lines.push({ points: currentLine });
+  currentLine.push({ x: cursor.x, y: cursor.y });
+
+  paint_canvas.dispatchEvent(new Event("drawing-changed"));
 });
 
 paint_canvas.addEventListener("mousemove", (e) => {
   if (cursor.active) {
-    ctx.beginPath();
-    ctx.moveTo(cursor.x, cursor.y);
-    ctx.lineTo(e.offsetX, e.offsetY);
-    ctx.stroke();
+    // ctx.beginPath();
+    // ctx.moveTo(cursor.x, cursor.y);
+    // ctx.lineTo(e.offsetX, e.offsetY);
+    // ctx.stroke();
     cursor.x = e.offsetX;
     cursor.y = e.offsetY;
+    currentLine.push({ x: cursor.x, y: cursor.y });
+    paint_canvas.dispatchEvent(new Event("drawing-changed"));
   }
 });
 
 paint_canvas.addEventListener("mouseup", () => {
   cursor.active = false;
 });
+
+paint_canvas.addEventListener("drawing-changed", () => {
+  console.log("Drawing changed");
+  redraw();
+});
+
+export default function redraw() {
+  ctx.clearRect(0, 0, paint_canvas.width, paint_canvas.height);
+  for (const line of lines) {
+    if (line.points.length > 1) {
+      ctx.beginPath();
+      const { x, y } = line[0];
+      ctx.moveTo(x, y);
+      for (const { x, y } of line.points) {
+        ctx.lineTo(x, y);
+      }
+      ctx.stroke();
+    }
+  }
+}
