16d8b10bd9eda8cf900b090db5150227f6c14103
Jaxon Ruiz <45025385+jaxonRuiz@users.noreply.github.com>
2024-10-21 18:00:04 -0700

    added mouse behavior to canvas

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,50 @@ import "./style.css";
 const APP_NAME = "Paint World";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 const title = document.createElement("h1")!;
-
-// setting up canvas
-const canvas = document.createElement("canvas")!;
-const ctx = canvas.getContext("2d")!;
-ctx.fillStyle = "white";
-
+const toolbar_container = document.createElement("div")!;
 document.title = APP_NAME;
 title.innerHTML = APP_NAME;
 
+// setting up canvas
+const paint_canvas = document.createElement("canvas")!;
+const ctx = paint_canvas.getContext("2d")!;
+paint_canvas.width = 256;
+paint_canvas.height = 256;
+ctx.fillStyle = "white";
+
+// adding clear button
+const clearButton = document.createElement("button")!;
+clearButton.innerHTML = "Clear";
+clearButton.addEventListener("click", () => {
+    ctx.clearRect(0, 0, paint_canvas.width, paint_canvas.height);
+});
+
+
 app.append(title);
-app.append(canvas);
+app.append(paint_canvas);
+app.append(toolbar_container);
+toolbar_container.append(clearButton);
+
+// setting up cursor behavior (mostly taken from glitch demo code)
+const cursor = { active: false, x: 0, y: 0 };
+
+paint_canvas.addEventListener("mousedown", (e) => {
+  cursor.active = true;
+  cursor.x = e.offsetX;
+  cursor.y = e.offsetY;
+});
+
+paint_canvas.addEventListener("mousemove", (e) => {
+  if (cursor.active) {
+    ctx.beginPath();
+    ctx.moveTo(cursor.x, cursor.y);
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    cursor.x = e.offsetX;
+    cursor.y = e.offsetY;
+  }
+});
+
+paint_canvas.addEventListener("mouseup", (e) => {
+  cursor.active = false;
+});
